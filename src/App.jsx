b65e2cc83ace268de1b442135eb5ab0bb795c97b
Dimitar Manastirsky <dmanastirski@gmail.com>
2025-06-11 13:25:25 +0200

    added layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
 import './App.css';
 
-import { Routes, Route } from "react-router-dom";
-import NavBar from './components/NavBar/NavBar';
-import Footer from './components/Footer/Footer';
-import Sidebar from './components/Sidebar/Sidebar';
+import { Routes, Route, BrowserRouter, Outlet } from "react-router-dom";
+
 import AnimalSummary from './pages/AnimalSummary/AnimalSummary';
 import AnimalDetails from './pages/AnimalDetails/AnimalDetails';
 
 import Home from './pages/Home/Home';
 import AnimalGroup from './pages/AnimalGroup/AnimalGroup';
 
+import NavBar from './components/NavBar/NavBar';
+import Footer from './components/Footer/Footer';
+import Sidebar from './components/Sidebar/Sidebar';
+
 function App() {
+
+  function Layout() {
+    return (
+      <>
+        <NavBar />
+        <Sidebar />
+        <div className='main-content'>
+              <Outlet />
+        </div>
+        <Footer />
+      </>
+    )
+  }
+
   return (
     <>
-      <NavBar />
-      <Sidebar />
-      <div className='main-content'>
-        <Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/mammals" element={<AnimalGroup groupType = "Mammals"/>} />
-          <Route path="/reptiles" element={<AnimalGroup groupType = "Reptiles" />} />
-          <Route path="/birds" element={<AnimalGroup groupType = "Birds" />} />
+          <Route path="/mammals" element={<AnimalGroup groupType="Mammals" />} />
+          <Route path="/reptiles" element={<AnimalGroup groupType="Reptiles" />} />
+          <Route path="/birds" element={<AnimalGroup groupType="Birds" />} />
 
           <Route path="/:id" element={<AnimalSummary />} />
           <Route path="/mammals/:id" element={<AnimalDetails />} />
           <Route path="/reptiles/:id" element={<AnimalDetails />} />
           <Route path="/birds/:id" element={<AnimalDetails />} />
-        </Routes>
-      </div>
-      <Footer />
+        </Route>
+
+      </Routes>
     </>
   )
 }
